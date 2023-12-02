6c8784eba6f7d41d2b548e89763a7167fb8e71f3
Vladyslav Rohalov <domus.lg@gmail.com>
2023-12-02 18:14:28 +0100

    Updated faq styles

--- src/app/components/faq/faq.jsx
+++ src/app/components/faq/faq.jsx
@@ -14,10 +14,10 @@ export default function Faq() {
   };
 
   return (
-    <section className={styles.faq_section} id="faq">
-      <h2 className={styles.faq_title}>Frequently Asked Questions</h2>
-      {accordionData.map((item, index) => {
-        return (
+    <section className="section" id="faq">
+      <div className={styles.container_mobile}>
+        <h2 className={styles.title}>Frequently Asked Questions</h2>
+        {accordionData.map((item, index) => (
           <AccordionItem
             key={item.id}
             item={item}
@@ -25,14 +25,37 @@ export default function Faq() {
             open={open}
             handleOpen={handleOpen}
           />
-        );
-      })}
-      <p className={styles.faq_text}>
-        Didn't find the answer to your question?{' '}
-      </p>
-      <button className={styles.faq_btn}>
-        Contact Us <IconDot />
-      </button>
+        ))}
+
+        <p className={styles.text}>Didn't find the answer to your question? </p>
+        <button className={styles.btn}>
+          Contact Us <IconDot />
+        </button>
+      </div>
+      <div className={styles.container_tablet}>
+        <div className={styles.column}>
+          {accordionData.map((item, index) => (
+            <AccordionItem
+              key={item.id}
+              item={item}
+              index={index}
+              open={open}
+              handleOpen={handleOpen}
+            />
+          ))}
+        </div>
+        <div className={styles.column}>
+          <h2 className={styles.title}>Frequently Asked Questions</h2>
+          <div className={styles.container_btn}>
+            <p className={styles.text}>
+              Didn't find the answer to your question?
+            </p>
+            <button className={styles.btn}>
+              Contact Us <IconDot />
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
